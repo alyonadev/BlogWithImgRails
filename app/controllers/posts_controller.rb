a2c142db2d1f7579f5022dd38e3574dfd92c878f
Aliona Kavalenka <aliona.kavalenka@itechart-group.com>
2022-06-22 16:33:45 +0300

    Add callback for post before action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,19 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
     @post = Post.new
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  def edit; end
 
   def create
-    @post = Post.new(post_params)
-
     if @post.save!
       redirect_to @post
     else
@@ -26,8 +22,6 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update_attributes(post_params)
       redirect_to @post
     else
@@ -36,12 +30,14 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
 
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
