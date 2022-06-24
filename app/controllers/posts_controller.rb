8bf0532dec563d13b53128a993025834789d49e2
Aliona Kavalenka <aliona.kavalenka@itechart-group.com>
2022-06-24 11:34:14 +0300

    Add summary to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,6 +43,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :image)
+    params.require(:post).permit(:title, :summary, :content, :image)
   end
 end
