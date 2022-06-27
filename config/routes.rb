ca43985ba90df047d5af640c0d70aa7d3dd4e9cf
Aliona Kavalenka <aliona.kavalenka@itechart-group.com>
2022-06-27 16:50:29 +0300

    Add post_controller test

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
 Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts
-  resources :pictures, only: [:create, :destroy]
+  resources :pictures, only: %i[create destroy]
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,33 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = Post.create(
+      title: 'New Post',
+      summary: 'Post summary',
+      content: 'some text',
+      image: ''
+    )
+  end
+
+  test 'should get index' do
+    get :index
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get 'posts#new'
+    assert_response :success
+  end
+
+  test 'should show post' do
+    visit post_path(@post)
+    assert_response :success
+  end
+
+  test 'post should be valid' do
+    assert @post.valid?
+  end
 end
