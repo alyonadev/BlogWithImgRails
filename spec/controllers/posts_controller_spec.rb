e65f4c549af255ccdc7651edcf6b7fd073728986
Aliona Kavalenka <aliona.kavalenka@itechart-group.com>
2022-06-29 12:01:03 +0300

    Update tests for post model and controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,10 +1,35 @@
 # frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'get index' do
-    it 'return a successful response' do
+  before do
+    @post = Post.create!(
+      title: 'New Post',
+      summary: 'Post summary',
+      content: 'some text',
+      image: ''
+    )
+  end
+
+  describe 'return a successful response' do
+    it 'get index' do
       get posts_path
+      expect(response).to be_successful
+    end
+
+    it 'get show' do
+      get post_path(@post)
+      expect(response).to be_successful
+    end
+
+    it 'get edit' do
+      get post_path(@post)
+      expect(response).to be_successful
+    end
+
+    it 'get new' do
+      get new_post_path
       expect(response).to be_successful
     end
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,8 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'is valid with valid attributes' do
-    post = Post.new(title: 'Test', summary: 'Test summary', content: 'Test content')
-    expect(post).to be_valid
+  describe 'is valid attributes' do
+    it 'valid all attributes' do
+      post = Post.new(title: 'Test', summary: 'Test summary', content: 'Test content')
+      expect(post).to be_valid
+    end
+
+    it 'valid without summary' do
+      post = Post.new(title: 'Test', content: 'Test content')
+      expect(post).to be_valid
+    end
+  end
+
+  describe 'is not valid with attributes' do
+    it 'is not valid without title' do
+      post = Post.new(summary: 'Test summary', content: 'Test content')
+      expect(post).to_not be_valid
+    end
+
+    it 'is not valid without content' do
+      post = Post.new(title: 'Test', summary: 'Test summary')
+      expect(post).to_not be_valid
+    end
+
+    it 'is not valid without dates' do
+      post = Post.new
+      expect(post).to_not be_valid
+    end
   end
 end
