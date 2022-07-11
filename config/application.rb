09363786fd173ccd1d4972761dd9672484d2a2e0
Aliona Kavalenka <aliona.kavalenka@itechart-group.com>
2022-07-11 13:36:08 +0300

    Set config

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module BlogWithImgRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
     config.assets.initialize_on_precompile = false
   end
 end
